839c79d1603abd109af589afe85114de24ba15bd
dono <xononeet@gmail.com>
2021-05-09 04:52:43 +0900

    Update niconico login

--- niconico.go
+++ niconico.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 	"os"
 	"strings"
@@ -19,14 +21,20 @@ func main() {
 		log.Fatal(err)
 	}
 
+	jar, err := cookiejar.New(&cookiejar.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mail := os.Getenv("NICONICO_MAIL")
 	pass := os.Getenv("NICONICO_PASS")
 
 	client := &http.Client{
-        CheckRedirect: func(req *http.Request, via []*http.Request) error {
-            return http.ErrUseLastResponse
-        },
-    }
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		 Jar:     jar,
+	}
 
 	form := url.Values{}
 	form.Add("mail_tel", mail)
@@ -41,23 +49,44 @@ func main() {
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	resp, err := client.Do(req)	
+	_, err = client.Do(req)	
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	userSession := ""
-	userSessionSecure := ""
-
-	for _, cookie := range resp.Cookies() {
-		if cookie.Name == "user_session" && cookie.Value != "deleted" {
-			userSession = cookie.Value
-		}
-		if cookie.Name == "user_session_secure" {
-			userSessionSecure = cookie.Value
-		}
+	req, err = http.NewRequest("GET", "https://com.nicovideo.jp/community/co3000390", body)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Println(userSession)
-	fmt.Println(userSessionSecure)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(bytes))
+
+	
+
+	// userSession := ""
+	// userSessionSecure := ""
+
+	// for _, cookie := range resp.Cookies() {
+	// 	if cookie.Name == "user_session" && cookie.Value != "deleted" {
+	// 		userSession = cookie.Value
+	// 	}
+	// 	if cookie.Name == "user_session_secure" {
+	// 		userSessionSecure = cookie.Value
+	// 	}
+	// }
+
+	// fmt.Println(userSession)
+	// fmt.Println(userSessionSecure)
 }
