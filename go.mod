688cc10c69842d3121b771b94a69c63c48c39cf4
dono <xononeet@gmail.com>
2021-05-09 21:16:22 +0900

    Update niconico.go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dono/live-checker
 
 go 1.16
 
-require github.com/joho/godotenv v1.3.0 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.6.1 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- niconico.go
+++ niconico.go
@@ -1,79 +1,64 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/http/cookiejar"
-	"net/url"
-	"os"
-	"strings"
-
-	"github.com/joho/godotenv"
-)
-
 
 const URL = `https://account.nicovideo.jp/login/redirector`
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal(err)
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	jar, err := cookiejar.New(&cookiejar.Options{})
-	if err != nil {
-		log.Fatal(err)
-	}
+	// jar, err := cookiejar.New(&cookiejar.Options{})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	mail := os.Getenv("NICONICO_MAIL")
-	pass := os.Getenv("NICONICO_PASS")
+	// mail := os.Getenv("NICONICO_MAIL")
+	// pass := os.Getenv("NICONICO_PASS")
 
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-		 Jar:     jar,
-	}
+	// client := &http.Client{
+	// 	CheckRedirect: func(req *http.Request, via []*http.Request) error {
+	// 		return http.ErrUseLastResponse
+	// 	},
+	// 	 Jar:     jar,
+	// }
 
-	form := url.Values{}
-	form.Add("mail_tel", mail)
-	form.Add("password", pass)
+	// form := url.Values{}
+	// form.Add("mail_tel", mail)
+	// form.Add("password", pass)
 
-	body := strings.NewReader(form.Encode())
+	// body := strings.NewReader(form.Encode())
 
-	req, err := http.NewRequest("POST", URL, body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// req, err := http.NewRequest("POST", URL, body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	// req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	_, err = client.Do(req)	
-	if err != nil {
-		log.Fatal(err)
-	}
+	// _, err = client.Do(req)	
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	req, err = http.NewRequest("GET", "https://com.nicovideo.jp/community/co3000390", body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// req, err = http.NewRequest("GET", "https://com.nicovideo.jp/community/co3000390", body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	defer resp.Body.Close()
+	// defer resp.Body.Close()
 
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// bytes, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Println(string(bytes))
-
-	
+	// fmt.Println(string(bytes))
 
 	// userSession := ""
 	// userSessionSecure := ""
@@ -89,4 +74,7 @@ func main() {
 
 	// fmt.Println(userSession)
 	// fmt.Println(userSessionSecure)
+
+   // https://com.nicovideo.jp/api/v1/communities/2021140/lives.json?limit=1&offset=0
+   // ↑で配信されてるかどうか一発で取れる件
 }
